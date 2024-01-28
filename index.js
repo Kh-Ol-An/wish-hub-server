09183080ae28954fd693012be17260296562425f
Oleg Khristenk <olegkhristenko@gmail.com>
2024-01-28 16:39:39 +0200

    Added params to cors

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 require('dotenv').config();
 const express = require('express');
-const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const router = require('./router/index');
 const errorMiddleware = require('./middlewares/error-middleware');
 
 const PORT = process.env.PORT || 5000;
+
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+}));
+//app.use(function(req, res, next) {
+//    res.header('Access-Control-Allow-Origin', req.headers.origin);
+//    res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,token");
+//    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+//    res.header("Content-Type", "application/json;charset=utf-8");
+//    res.header ("Access-Control-Allow-Credentials", true);
+//    next();
+//});
 app.use('/api', router);
 app.use(errorMiddleware);
 
@@ -25,4 +37,3 @@ const start = async () => {
 };
 
 start();
-// 1.13.00
