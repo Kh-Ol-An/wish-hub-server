1f6459345f7d0b660435a7a8d5537c4a7121f840
Oleg Khristenk <olegkhristenko@gmail.com>
2024-02-17 12:49:13 +0200

    added express.json to app use

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-//app.use(express.json());
+app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
