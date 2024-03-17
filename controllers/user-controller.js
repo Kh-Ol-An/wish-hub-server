d4e40137bd4ebd3bd053a5dfbe734f2f5fdb1f8a
Oleg Khristenk <olegkhristenko@gmail.com>
2024-03-17 15:07:18 +0200

    added console.log to controller refresh

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -78,6 +78,7 @@ class UserController {
     async refresh(req, res, next) {
         try {
             const { refreshToken } = req.cookies;
+            console.log('req.cookies: ', req.cookies);
             const userData = await userService.refresh(refreshToken);
 
             res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
