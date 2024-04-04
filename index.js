6f2826f107227c1502cb59fa6f41122095e13e87
Oleg Khristenk <olegkhristenko@gmail.com>
2024-04-04 19:51:36 +0300

    added console log

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ cron.schedule(
 );
 
 const start = async () => {
+    console.log('Start server: ', new Date(Date.now()));
     try {
         await mongoose.connect(process.env.DB_URL);
         app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
