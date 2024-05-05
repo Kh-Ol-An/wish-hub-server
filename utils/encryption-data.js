35f6154e7b157db2d27e2a79e856b8a586e3a1c9
kholan <olegkhristenko@gmail.com>
2024-05-05 09:52:50 +0300

    fail crypt

--- utils/encryption-data.js
+++ utils/encryption-data.js
@@ -1,20 +1,23 @@
 const CryptoJS = require('crypto-js');
 
-const encryptData = data => CryptoJS.AES.encrypt(data, process.env.CRYPTO_JS_SECRET).toString();
+// const encryptData = data => CryptoJS.AES.encrypt(data, process.env.CRYPTO_JS_SECRET).toString();
+const encryptData = data => data;
 
-const decryptData = data => {
-    console.log('decryptData')
-    try {
-        const decrypted = CryptoJS.AES.decrypt(data, process.env.CRYPTO_JS_SECRET).toString(CryptoJS.enc.Utf8);
-        console.log('decrypted: ', decrypted);
-        if (!decrypted) {
-            throw new Error('Decryption failed');
-        }
-        return decrypted;
-    } catch (error) {
-        return data;
-    }
-};
+// const decryptData = data => {
+//     console.log('decryptData')
+//     try {
+//         const decrypted = CryptoJS.AES.decrypt(data, process.env.CRYPTO_JS_SECRET).toString(CryptoJS.enc.Utf8);
+//         console.log('decrypted: ', decrypted);
+//         if (!decrypted) {
+//             throw new Error('Decryption failed');
+//         }
+//         return decrypted;
+//     } catch (error) {
+//         return data;
+//     }
+// };
+
+const decryptData = data => data;
 
 module.exports = {
     encryptData,
