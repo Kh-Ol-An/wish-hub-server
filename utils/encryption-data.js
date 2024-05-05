a1cae3aa400dfe82156e56fdd905d7cab683790a
kholan <olegkhristenko@gmail.com>
2024-05-05 09:27:35 +0300

    testing crypt 2

--- utils/encryption-data.js
+++ utils/encryption-data.js
@@ -3,8 +3,10 @@ const CryptoJS = require('crypto-js');
 const encryptData = data => CryptoJS.AES.encrypt(data, process.env.CRYPTO_JS_SECRET).toString();
 
 const decryptData = data => {
+    console.log('decryptData')
     try {
         const decrypted = CryptoJS.AES.decrypt(data, process.env.CRYPTO_JS_SECRET).toString(CryptoJS.enc.Utf8);
+        console.log('decrypted: ', decrypted);
         if (!decrypted) {
             throw new Error('Decryption failed');
         }
