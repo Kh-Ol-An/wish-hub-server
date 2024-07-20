3c26cd5f04048ef07cccf39c5bde447b9282087f
KhOlAn <olegkhristenko@gmail.com>
2024-07-20 16:58:46 +0300

    fixed convert-to-base-currency.js

--- utils/convert-to-base-currency.js
+++ utils/convert-to-base-currency.js
@@ -2,11 +2,23 @@ const axios = require('axios');
 
 async function getExchangeRates() {
     const response = await axios.get(process.env.CURRENCY_EXCHANGE_URL);
-    if (response.data.result === 'success') {
-        return response.data.conversion_rates;
+    // ***** fixer.io ***** //
+    if (response.data.success) {
+        return response.data.rates;
     } else {
         throw new Error('Failed to fetch exchange rates');
     }
+    // ***** fixer.io ***** //
+
+    // ========================================================================================= //
+
+    // ***** exchangerate-api.com ***** //
+    // if (response.data.result === 'success') {
+    //     return response.data.conversion_rates;
+    // } else {
+    //     throw new Error('Failed to fetch exchange rates');
+    // }
+    // ***** exchangerate-api.com ***** //
 }
 
 const convertToBaseCurrency = async (price, currency) => {
