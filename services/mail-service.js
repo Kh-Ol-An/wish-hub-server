872c1df36a0b904b639645bc4b5bae6b39d1ac3d
KhOlAn <olegkhristenko@gmail.com>
2024-07-23 20:39:21 +0300

    changed email provider from ukr.net to gmail.com

--- services/mail-service.js
+++ services/mail-service.js
@@ -4,9 +4,10 @@ const fs = require('fs').promises;
 class MailService {
     constructor() {
         this.transporter = nodemailer.createTransport({
+            service: process.env.SMTP_SERVICE,
             host: process.env.SMTP_HOST,
             port: process.env.SMTP_PORT,
-            // secure: true,
+            secure: true,
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASSWORD
