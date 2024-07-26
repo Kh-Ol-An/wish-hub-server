9bcf5fbec7b2520234d22924482186282c36ba3a
KhOlAn <olegkhristenko@gmail.com>
2024-07-26 15:13:08 +0300

    fixed getImageId

--- utils/get-image-id.js
+++ utils/get-image-id.js
@@ -1,7 +1,7 @@
 const getImageId = (url) => {
     const urlWithoutProtocol = url.replace('https://', '');
-    const imageName = urlWithoutProtocol.split('/')[4];
-    return imageName.split('_')[0];
+    const imageName = urlWithoutProtocol?.split('/')[4];
+    return imageName?.split('_')[0];
 };
 
 module.exports = getImageId;
